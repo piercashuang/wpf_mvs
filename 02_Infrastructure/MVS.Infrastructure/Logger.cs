58f60e419d9c738106f62060b890ef395093f4dd
zhanyinhao <1440090140@qq.com>
2026-04-01 13:29:36 +0800

    文件夹创建
    合并

---
+++ 02_Infrastructure/MVS.Infrastructure/Logger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVS.Infrastructure.Log
+{
+    public static class Logger
+    {
+        public static void Info(string message)
+        {
+            // 暂时先输出到控制台，以后可以改写成存入文件
+            Console.WriteLine($"[{DateTime.Now:HH:mm:ss}] [INFO] {message}");
+        }
+    }
+}
